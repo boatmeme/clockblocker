a20acbddef85b0d090920b2b6b134ec78c38274f
Jonathan Griggs <jonathan.griggs@gmail.com>
2022-07-07 01:03:14 -0600

    Establish jest timer mocking pattern

--- test/Clock.test.ts
+++ test/Clock.test.ts
@@ -1,6 +1,14 @@
 import { Clock } from '../src/index';
 
 describe(`Clock class`, () => {
+  const currentDate = new Date();
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(currentDate);
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
   describe(`constructor`, () => {
     it('exists', () => {
       const clock = new Clock();
@@ -10,8 +18,15 @@ describe(`Clock class`, () => {
   describe(`getRealTime`, () => {
     it('returns the real time', () => {
       const clock = new Clock();
-      const now = clock.realTimeInMillis;
-      expect(typeof now).toBe('number');
+      const start = clock.realTimeInMillis;
+      expect(typeof start).toBe('number');
+      expect(start).toEqual(currentDate.getTime());
+
+      jest.advanceTimersByTime(1000);
+
+      const end = clock.realTimeInMillis;
+      expect(typeof end).toBe('number');
+      expect(end - start).toEqual(1000);
     });
   });
 });
